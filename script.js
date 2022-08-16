8d351c4a462fb95983ceb29a17aa6622ca74a73d
rajesh <krajeshgowri7032@mail.com>
2022-08-16 16:28:31 +0530

    javascript-Assign

---
+++ script.js
@@ -0,0 +1,12 @@
+//Assign
+console.log(x=7);
+//Add and Assign
+console.log(x+=7);
+//sub and Assign
+console.log(x-=7);
+//mul and Assign
+console.log(x*=7);
+// Div and Assign
+console.log(x/=7);
+//Modulus Assign
+console.log(x%=7);
